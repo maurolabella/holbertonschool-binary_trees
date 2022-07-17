790125a817e44a16bee43d745948fece3a03ff46
maurolabella <maurolabella@yahoo.com>
2022-07-17 19:52:42 -0300

    task-0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * createnode - creates nodes for binary trees
+ *
+ * @value: integer to be incorporated to the new node
+ * Return: binary_tree_t*
+ */
+
+binary_tree_t *createnode(int value)
+{
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+
+	if (node)
+	{
+		node->n = value;
+		node->parent = NULL;
+		node->left = NULL;
+		node->right = NULL;
+	}
+	return (node);
+}
+
+/**
+ * binary_tree_node - integrates new node to a binary tree
+ *
+ * @parent: parent node
+ * @value: value to be sent to proper creation function
+ * Return: binary_tree_t*
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = createnode(value);
+
+	node->parent = parent;
+	return (node);
+}
---
+++ 0-main.d
@@ -0,0 +1,1 @@
+obj/0-main.o: 0-main.c binary_trees.h
