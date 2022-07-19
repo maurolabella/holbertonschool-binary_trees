a6ee96f673c74282a5f6d63ea317231461f57fcd
maurolabella <maurolabella@yahoo.com>
2022-07-18 23:35:24 -0300

    a

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,6 +12,9 @@ static size_t _height_bal(const binary_tree_t *tree)
 	size_t height_l;
 	size_t height_r;
 
+	if (!tree)
+		return (0);
+
 	height_l = tree->left ? 1 + (int)_height_bal(tree->left) : 0;
 	height_r = tree->right ? 1 + (int)_height_bal(tree->right) : 0;
 	return (height_l - height_r);
