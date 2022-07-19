ebde59b7c3e6dace19112a3db57163f8048fcdea
maurolabella <maurolabella@yahoo.com>
2022-07-18 23:14:46 -0300

    a

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * _height-bal - Measures the height of a binary tree
+ *
+ * @tree: Pointer to the node to measures the height
+ *
+ * Return: The height of the tree starting at @node
+ */
+static size_t _height_bal(const binary_tree_t *tree)
+{
+	size_t height_l;
+	size_t height_r;
+
+	height_l = tree->left ? 1 + _height_bal(tree->left) : 0;
+	height_r = tree->right ? 1 + _height_bal(tree->right) : 0;
+	return (height_l - height_r);
+}
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: input tree
+ * Return: balance factor of descendants
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (_height_bal(tree));
+}
--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ CXXFLAGS = -g -Wall -Werror -Wextra -pedantic -std=gnu89
 LDFLAGS = 
 
 # Makefile settings - Can be customized.
-APPNAME = bin/13-nodes
+APPNAME = bin/14-balance
 EXT = .c
 SRCDIR = .
 OBJDIR = ./obj
