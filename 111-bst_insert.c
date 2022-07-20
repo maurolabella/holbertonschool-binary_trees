a3b86ba097e8d045cb17cbdaf3139e9d7e6eb2de
maurolabella <maurolabella@yahoo.com>
2022-07-20 13:26:09 -0300

    a

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,6 +1,32 @@
 #include "binary_trees.h"
 
 /**
+ * _bst_insert - inserts value in a binary search tree
+ *
+ * @tree: tree root
+ * @value: newnode's value
+ * @root: parent's address
+ * Return: bst_t* pointer
+ */
+bst_t *_bst_insert(bst_t **tree, int value, bst_t *root)
+{
+	bst_t *res = NULL;
+
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(root, value);
+		return (*tree);
+	}
+	if (value == (*tree)->n)
+		return (NULL);
+	if (value < (*tree)->n)
+		res = (_bst_insert(&((*tree)->left), value, *tree));
+	if (value > (*tree)->n)
+		res = (_bst_insert(&((*tree)->right), value, *tree));
+	return (res);
+}
+
+/**
  * bst_insert - inserts value in a binary search tree
  *
  * @tree: tree root
@@ -9,16 +35,16 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *root, *result = NULL;
+	bst_t *res, *root = *tree;
 
-	if (*tree == NULL)
-		return (binary_tree_node(*tree, value));
-	root = *tree;
-	if (value == root->n)
-		return (result);
-	if (value < root->n)
-		result = (bst_insert(&(root->left), value));
-	if (value > root->n)
-		result = (bst_insert(&(root->right), value));
-	return (result);
+	if (root == NULL)
+	{
+		(*tree) = binary_tree_node(root, value);
+		return (*tree);
+	}
+	else
+	{
+		res = _bst_insert(tree, value, root);
+		return (res);
+	}
 }
